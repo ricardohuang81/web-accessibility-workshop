3b8257974f027b435a88f9fd3979b9ca8510704b
Ricky Huang <huangricky@gmail.com>
2019-09-04 11:39:20 -0400

    made the changes from slides besides contrasting colors

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
+
+
 $(document).ready(function () {
   $('.js-banner .close').on('click', function (e) {
-    $('.banner').addClass('hide');
+    $('.banner').hide();
   });
 
   $('.section-aside .js-link').on('click', function (e) {
     window.open('https://github.com/kiss/web-accessibility-workshop');
   });
+
+  function handleKeyDown(event) {
+    if (event.keyCode == 13) {
+      $(event.target).prop('checked') ?
+      $(event.target).prop('checked', false) :
+      $(event.target).prop('checked', true);
+    }
+  }
+
+  const inputCheckbox = document.querySelectorAll('input[type=checkbox]');
+
+  if (inputCheckbox.length) {
+    for (let i = 0; i < inputCheckbox.length; i++) {
+      inputCheckbox[i].addEventListener('keydown', handleKeyDown);
+    }
+  }
 
   $('.form-subscribe .button').on('click', function (e) {
     const $inputs = $('.form-subscribe input');
